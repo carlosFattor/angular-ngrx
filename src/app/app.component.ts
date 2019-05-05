58bb79dc17df1183a9e3078e23c1489193fece03
Carlos <carlos.fattor@gmail.com>
2019-05-05 00:32:03 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  logout() {
+  }
+}
